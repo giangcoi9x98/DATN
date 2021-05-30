6cf51c5aefa86208f81bbc40faebe509b59861eb
GiangTran <giangcoi9x98@gmail.com>
2021-05-30 22:56:57 +0700

    refactor action upload

--- Back-end/src/services/post.service.js
+++ Back-end/src/services/post.service.js
@@ -2,6 +2,7 @@
 const CommonMixin = require("../mixins/common.mixin");
 const ResponseData = require("../lib/response");
 const { MoleculerError } = require("moleculer").Errors;
+const uuid = require("uuid").v4;
 
 module.exports = {
 	name: "post",
@@ -26,30 +27,33 @@ module.exports = {
 				limit: "number|optional",
 			},
 			async handler(ctx) {
-				try {
-					await this.mysql.query("START TRANSACTION;");
+				const conn = await this.mysql.beginTransaction();
 
+				try {
 					const data = await this.mysql
-						.queryMulti("SELECT * FROM post")
-						.then((res) => res);
-					await this.mysql.query("SET autocommit = 0");
+						.queryMulti(
+							"SELECT * FROM post ORDER BY update_at DESC",
+							[],
+							conn
+						);
 					const result = await Promise.all(
 						data.map(async (element) => {
 							const comment = await this.mysql
 								.queryMulti(
 									"SELECT * FROM comment WHERE postId =?",
-									[element.id]
-								)
-								.then((res) => res);
+									[element.id],
+									conn
+								);
 							const like = await this.mysql
 								.queryMulti(
 									"SELECT * FROM like_post WHERE postId = ?",
-									[element.id]
-								)
-								.then((res) => res);
+									[element.id],
+									conn
+								);
 							const file = await this.mysql.queryMulti(
 								"SELECT * FROM file WHERE postId = ?",
-								[element.id]
+								[element.id],
+								conn
 							);
 							element.totalLike = like.length;
 							element.likes = like;
@@ -63,11 +67,11 @@ module.exports = {
 					);
 					// console.log("el", res);
 					console.log("data", result[0]);
-					await this.mysql.query("COMMIT;");
+					await this.mysql.commitTransaction(conn);
 					return new ResponseData(true, "SUCCESS", result);
 				} catch (error) {
 					this.logger.error("Error at getAllPost action ", error);
-					await this.mysql.query("ROLLBACK");
+					await this.mysql.rollbackTransaction(conn);
 					throw error;
 				}
 			},
@@ -83,53 +87,56 @@ module.exports = {
 					type: "string",
 					require: true,
 				},
-				img: "string|optional",
+				img: "array|optional",
 			},
 			async handler(ctx) {
+				const conn = await this.mysql.beginTransaction();
 				try {
 					const { user } = ctx.meta;
 					const { content, img } = ctx.params;
-					this.socketBroadcast({
-						event: "NEW_MESSAGE",
-						args: [
-							{
-								name: "giang",
-							},
-						],
-					});
-					await this.mysql.query("START TRANSACTION");
-					const res = await this.mysql.query(
-						"INSERT INTO post(accountId, content) VALUES(?, ?)",
-						[user.id, content]
+					let id = uuid();
+					// this.socketBroadcast({
+					// 	event: "NEW_MESSAGE",
+					// 	args: [
+					// 		{
+					// 			name: "giang",
+					// 		},
+					// 	],
+					// });
+					const res = await conn.query(
+						"INSERT INTO post(id, accountId, content) VALUES(?, ?, ?)",
+						[id, user.id, content]
 					);
 
-					if (img) {
-						console.log("insert with img");
-						const file = await this.mysql.query(
-							`
-						INSERT INTO file(accountId, filename, path, postId)
-						VALUES(?, ?, ?, ?)`,
-							[
-								user.id,
-								"img",
-								`${user.email + "/" + img}`,
-								res.insertId,
-							]
-						);
-						await this.mysql.query(
-							"UPDATE post SET fileId = ? WHERE id = ?",
-							[file.insertId, res.insertId]
+					if (img.length) {
+						console.log("img", img);
+						await Promise.all(
+							img.map(async e => {
+								let fileId = uuid();
+								await conn.query(
+									`
+								INSERT INTO file(id, accountId, filename, path, postId)
+								VALUES(?,?, ?, ?, ?)`,
+									[
+										fileId,
+										user.id,
+										"img",
+										`${user.email + "/" + e}`,
+										id,
+									]
+								);
+							})
 						);
 					}
-					const data = await this.mysql.query(
+					const data = await conn.query(
 						"SELECT * FROM post WHERE id = ?",
-						[res.insertId]
+						[id]
 					);
-					await this.mysql.query("COMMIT");
-					return new ResponseData(true, "Success", data);
+					await this.mysql.commitTransaction(conn);
+					return new ResponseData(true, "Success", data.data);
 				} catch (error) {
 					this.logger.error("Error at newPost action ", error);
-					await this.mysql.query("ROLLBACK");
+					await this.mysql.rollbackTransaction(conn);
 					throw error;
 				}
 			},
